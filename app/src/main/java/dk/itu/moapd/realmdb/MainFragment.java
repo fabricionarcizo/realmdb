dd8a91547c3c049aec6955f4060c4191ea6ee772
Fabricio Batista Narcizo <fabn@itu.dk>
2020-03-02 21:52:23 +0100

    Using RealmRecyclerViewAdapter

--- app/src/main/java/dk/itu/moapd/realmdb/MainFragment.java
+++ app/src/main/java/dk/itu/moapd/realmdb/MainFragment.java
@@ -4,10 +4,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import io.realm.OrderedRealmCollection;
 import io.realm.Realm;
+import io.realm.RealmRecyclerViewAdapter;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
@@ -23,9 +27,7 @@ public class MainFragment extends Fragment {
     private EditText mEditText;
 
     private View mPrevSelected;
-
-    private ActorNameAdapter mAdapter;
-    private int mId;
+    private ActorName mActorName;
 
     private Realm mRealm;
 
@@ -57,8 +59,6 @@ public class MainFragment extends Fragment {
                         }
                     }
                 });
-
-                updateUI();
             }
         });
 
@@ -71,18 +71,15 @@ public class MainFragment extends Fragment {
                     public void execute(@NonNull Realm realm) {
                         String text = mEditText.getText().toString();
                         if (!text.isEmpty()) {
-                            ActorName actorName = realm.where(ActorName.class)
-                                    .equalTo("id", mId).findFirst();
-                            if (actorName != null) {
-                                actorName.setName(text);
-                                realm.copyToRealm(actorName);
+                            if (mActorName != null) {
+                                mActorName.setName(text);
+                                realm.copyToRealm(mActorName);
                                 mEditText.setText("");
+                                mActorName = null;
                             }
                         }
                     }
                 });
-
-                updateUI();
             }
         });
 
@@ -93,97 +90,57 @@ public class MainFragment extends Fragment {
                 mRealm.executeTransaction(new Realm.Transaction() {
                     @Override
                     public void execute(@NonNull Realm realm) {
-                        ActorName actorName = realm.where(ActorName.class)
-                                .equalTo("id", mId).findFirst();
-                        if (actorName != null) {
-                            actorName.deleteFromRealm();
+                        if (mActorName != null) {
+                            mActorName.deleteFromRealm();
                             mEditText.setText("");
+                            mActorName = null;
                         }
                     }
                 });
-
-                updateUI();
             }
         });
 
-        mAdapter = new ActorNameAdapter();
+        RealmResults<ActorName> results =
+                mRealm.where(ActorName.class)
+                      .sort("id", Sort.ASCENDING)
+                      .findAllAsync();
+        ActorNameAdapter mAdapter = new ActorNameAdapter(results);
+
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(mAdapter);
 
-        updateUI();
-
         return view;
     }
 
-    private void updateUI() {
-        mRealm.executeTransaction(new Realm.Transaction() {
-            @Override
-            public void execute(@NonNull Realm realm) {
-                RealmResults<ActorName> results =
-                        realm.where(ActorName.class)
-                             .sort("id", Sort.ASCENDING)
-                             .findAll();
-                mAdapter.setActorNames(results);
-            }
-        });
-
-        mId = -1;
-    }
-
     private class ActorNameAdapter
-            extends RecyclerView.Adapter<ActorNameHolder> {
+            extends RealmRecyclerViewAdapter<ActorName, ActorNameHolder> {
 
-        private RealmResults<ActorName> mActorNames;
+
+        ActorNameAdapter(@Nullable OrderedRealmCollection<ActorName> data) {
+            super(data, true);
+        }
 
         @NonNull
         @Override
         public ActorNameHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater =
-                    LayoutInflater.from(parent.getContext());
-            return new ActorNameHolder(layoutInflater, parent);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_actor_name, parent, false);
+            //itemView = view;
+            return new ActorNameHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull ActorNameHolder holder, int position) {
-            if (mActorNames != null && mActorNames.size() > position) {
-                ActorName actorName = mActorNames.get(position);
-                assert actorName != null;
-                holder.bind(actorName);
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            if (mActorNames != null)
-                return mActorNames.size();
-            return 0;
-        }
-
-        void setActorNames(RealmResults<ActorName> actorNames) {
-            mActorNames = actorNames;
-            notifyDataSetChanged();
-        }
-
-    }
-
-    private class ActorNameHolder
-            extends RecyclerView.ViewHolder {
-
-        private int id;
-        private final TextView mActorNameView;
-
-        ActorNameHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.list_actor_name, parent, false));
-            mActorNameView = itemView.findViewById(R.id.actor_name);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
+            final ActorName actorName = getItem(position);
+            assert actorName != null;
+            holder.mActorNameView.setText(actorName.getName());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
-                    String text = mActorNameView.getText().toString();
+                    String text = actorName.getName();
                     mEditText.setText(text);
-                    mId = id;
+                    mActorName = actorName;
 
                     if (mPrevSelected != null)
                         mPrevSelected.setBackgroundColor(view.getDrawingCacheBackgroundColor());
@@ -194,9 +151,15 @@ public class MainFragment extends Fragment {
             });
         }
 
-        void bind(ActorName actorName) {
-            id = actorName.getId();
-            mActorNameView.setText(actorName.getName());
+    }
+
+    private class ActorNameHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mActorNameView;
+
+        ActorNameHolder(View view) {
+            super(view);
+            mActorNameView = view.findViewById(R.id.actor_name);
         }
 
     }
